d12368b8f751850824f9031854d2def52ece3b1d
12ys0c001 <45426727+12ys0c001@users.noreply.github.com>
2019-05-06 09:31:52 +0800

    Update HelloWorld.java

--- Cgb1812/src/test/HelloWorld.java
+++ Cgb1812/src/test/HelloWorld.java
@@ -2,11 +2,8 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("helloworld");
-
-		System.out.println("111111111111111");
-
-		System.out.println("22222222222222222");
+	int a =1;
+	int b =2;	
 		
 		
 	}
