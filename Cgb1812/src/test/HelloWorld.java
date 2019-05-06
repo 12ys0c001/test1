f2b38acf8e787020131611a51b761caf626bfbb0
11203 <11203@192.168.203.1>
2019-05-06 09:35:26 +0800

    aa

--- Cgb1812/src/test/HelloWorld.java
+++ Cgb1812/src/test/HelloWorld.java
@@ -2,11 +2,7 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("helloworld");
-
-		System.out.println("111111111111111");
-
-		System.out.println("22222222222222222");
+		String name = "lzy";
 		
 		
 	}
